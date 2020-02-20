2137efbeddd0da81d8d5aad778246759043c5834
Groshev Anton <groshev.av1605@gmail.com>
2020-02-20 10:17:14 +0500

    Условие в тело записки

--- src/App.js
+++ src/App.js
@@ -1,141 +1,142 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import ru from 'date-fns/locale/ru';
+import ru from "date-fns/locale/ru";
 
-import './App.css';
-import 'materialize-css/dist/css/materialize.min.css'
+import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [state, setState] = useState({
-    from: 'Зав. кафедрой КБМОИС',
-    to: 'Начальнику учебного отдела',
-    theme: 'О переносе занятий',
+    from: "Зав. кафедрой КБМОИС",
+    to: "Начальнику учебного отдела",
+    theme: "О переносе занятий",
     date: new Date(),
-    fromName: 'Влацкой И.В.',
-    toName: 'Зинюхиной Н.А.',
-    footerWho: 'Зав. кафедрой КБМОИС',
-    footerWhoName: 'И. В. Влацкая'
-  })
+    fromName: "Влацкой И.В.",
+    toName: "Зинюхиной Н.А.",
+    footerWho: "Зав. кафедрой КБМОИС",
+    footerWhoName: "И. В. Влацкая"
+  });
 
-  const handleChange = (e) => {
-    const value = e.target.value
+  const handleChange = e => {
+    const value = e.target.value;
     setState({
       ...state,
       [e.target.name]: value
-    })
-  }
+    });
+  };
 
-  const handleChangeDate = (date) => {
+  const handleChangeDate = date => {
     setState({
       date
-    })
-  }
+    });
+  };
+
+  const RescheduleMemo = () => {
+    return <p>RescheduleMemo</p>;
+  };
+
+  const SubstitutingMemo = () => {
+    return <p>SubstitutingMemo</p>;
+  };
 
   const renderHeader = () => {
     return (
-      <div className='header'>
+      <div className="header">
         <div>
           <p>СЛУЖЕБНАЯ ЗАПИСКА</p>
         </div>
-        <div className='header__fields'>
-          <div className='header__fields__item'>
-            <p className='header__fields__item__title'>
-              Кому:
-              </p>
+        <div className="header__fields">
+          <div className="header__fields__item">
+            <p className="header__fields__item__title">Кому:</p>
             <input
-              type='text'
+              type="text"
               value={state.to}
               onChange={handleChange}
-              name='to'
+              name="to"
             />
             <input
-              type='text'
+              type="text"
               value={state.toName}
               onChange={handleChange}
-              name='toName'
+              name="toName"
             />
           </div>
-          <div className='header__fields__item'>
-            <p className='header__fields__item__title'>
-              От кого:
-              </p>
+          <div className="header__fields__item">
+            <p className="header__fields__item__title">От кого:</p>
             <input
-              type='text'
+              type="text"
               value={state.from}
               onChange={handleChange}
-              name='from'
+              name="from"
             />
             <input
-              type='text'
+              type="text"
               value={state.fromName}
               onChange={handleChange}
-              name='fromName'
+              name="fromName"
             />
           </div>
-          <div className='header__fields__item'>
-            <p className='header__fields__item__title'>
-              Дата:
-              </p>
+          <div className="header__fields__item">
+            <p className="header__fields__item__title">Дата:</p>
             <DatePicker
               selected={state.date}
               locale={ru}
               onChange={handleChangeDate}
-              name='date'
-              dateFormat={'dd.MM.yyyy'}
+              name="date"
+              dateFormat={"dd.MM.yyyy"}
             />
           </div>
-          <div className='header__fields__item'>
-            <p className='header__fields__item__title'>
-              Тема:
-              </p>
-            <select name='theme' onChange={handleChange} className='browser-default header__select' >
+          <div className="header__fields__item">
+            <p className="header__fields__item__title">Тема:</p>
+            <select
+              name="theme"
+              onChange={handleChange}
+              className="browser-default header__select"
+            >
               <option>О переносе занятий</option>
               <option>О замене преподавателя</option>
             </select>
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   const renderFooter = () => {
     return (
-      <div className='footer'>
+      <div className="footer">
         <div>
-          <div className='footer__inputs'>
+          <div className="footer__inputs">
             <input
-              type='text'
+              type="text"
               value={state.footerWho}
               onChange={handleChange}
-              name='footerWho'
+              name="footerWho"
             />
             <input
-              type='text'
+              type="text"
               value={state.footerWhoName}
               onChange={handleChange}
-              name='footerWhoName'
+              name="footerWhoName"
             />
           </div>
           <a className="waves-effect waves-light btn">сохранить записку</a>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div className="App">
-      <div className='wrapper'>
-        {
-          renderHeader()
-        }
-        <div className='main'>
-          main template
-          здесь тернарником сделать условие если выбрана "о переносе занятий", то ее конструктор, иначе другой
+      <div className="wrapper">
+        {renderHeader()}
+        <div className="main">
+          {state.theme === "О переносе занятий"
+            ? RescheduleMemo()
+            : SubstitutingMemo()}
         </div>
-        {
-          renderFooter()
-        }
+        {renderFooter()}
       </div>
     </div>
   );
