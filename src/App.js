07746b64105b3fc557c249d1788394dceab630b5
aleksej <al1111997@yandex.ru>
2020-02-20 09:27:22 +0500

    Добавлен футер записок

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ function App() {
     theme: 'О переносе занятий',
     date: new Date(),
     fromName: 'Влацкой И.В.',
-    toName: 'Зинюхиной Н.А.'
+    toName: 'Зинюхиной Н.А.',
+    footerWho: 'Зав. кафедрой КБМОИС',
+    footerWhoName: 'И. В. Влацкая'
   })
-  
+
   const handleChange = (e) => {
     const value = e.target.value
     setState({
@@ -30,81 +32,110 @@ function App() {
     })
   }
 
+  const renderHeader = () => {
+    return (
+      <div className='header'>
+        <div>
+          <p>СЛУЖЕБНАЯ ЗАПИСКА</p>
+        </div>
+        <div className='header__fields'>
+          <div className='header__fields__item'>
+            <p className='header__fields__item__title'>
+              Кому:
+              </p>
+            <input
+              type='text'
+              value={state.to}
+              onChange={handleChange}
+              name='to'
+            />
+            <input
+              type='text'
+              value={state.toName}
+              onChange={handleChange}
+              name='toName'
+            />
+          </div>
+          <div className='header__fields__item'>
+            <p className='header__fields__item__title'>
+              От кого:
+              </p>
+            <input
+              type='text'
+              value={state.from}
+              onChange={handleChange}
+              name='from'
+            />
+            <input
+              type='text'
+              value={state.fromName}
+              onChange={handleChange}
+              name='fromName'
+            />
+          </div>
+          <div className='header__fields__item'>
+            <p className='header__fields__item__title'>
+              Дата:
+              </p>
+            <DatePicker
+              selected={state.date}
+              locale={ru}
+              onChange={handleChangeDate}
+              name='date'
+              dateFormat={'dd.MM.yyyy'}
+            />
+          </div>
+          <div className='header__fields__item'>
+            <p className='header__fields__item__title'>
+              Тема:
+              </p>
+            <select name='theme' onChange={handleChange} className='browser-default header__select' >
+              <option>О переносе занятий</option>
+              <option>О замене преподавателя</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <div className='footer'>
+        <div>
+          <div className='footer__inputs'>
+            <input
+              type='text'
+              value={state.footerWho}
+              onChange={handleChange}
+              name='footerWho'
+            />
+            <input
+              type='text'
+              value={state.footerWhoName}
+              onChange={handleChange}
+              name='footerWhoName'
+            />
+          </div>
+          <a className="waves-effect waves-light btn">сохранить записку</a>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <div className='wrapper'>
-        <div className='header'>
-          <div>
-            <p>СЛУЖЕБНАЯ ЗАПИСКА</p>
-          </div>
-          <div className='header__fields'>
-            <div className='header__fields__item'>
-              <p className='header__fields__item__title'>
-                Кому:
-              </p>
-              <input
-                type='text'
-                value={state.to}
-                onChange={handleChange}
-                name='to'
-              />
-              <input
-                type='text'
-                value={state.toName}
-                onChange={handleChange}
-                name='toName'
-              />
-            </div>
-            <div className='header__fields__item'>
-              <p className='header__fields__item__title'>
-                От кого:
-              </p>
-              <input
-                type='text'
-                value={state.from}
-                onChange={handleChange}
-                name='from'
-              />
-              <input
-                type='text'
-                value={state.fromName}
-                onChange={handleChange}
-                name='fromName'
-              />
-            </div>
-            <div className='header__fields__item'>
-              <p className='header__fields__item__title'>
-                Дата:
-              </p>
-              <DatePicker
-                selected={state.date}
-                locale={ru}
-                onChange={handleChangeDate}
-                name='date'
-                dateFormat={'dd.MM.yyyy'}
-              />
-            </div>
-            <div className='header__fields__item'>
-              <p className='header__fields__item__title'>
-                Тема:
-              </p>
-              <select name='theme' onChange={handleChange} className='browser-default header__select' >
-                <option>О переносе занятий</option>
-                <option>О замене преподавателя</option>
-              </select>
-            </div>
-          </div>
-        </div>
+        {
+          renderHeader()
+        }
         <div className='main'>
-           main template
-           здесь тернарником сделать условие если выбрана "о переносе занятий", то ее конструктор, иначе другой
+          main template
+          здесь тернарником сделать условие если выбрана "о переносе занятий", то ее конструктор, иначе другой
         </div>
-        <div className='footer'>
-          <div>
-            footer template <br />
-            <a className="waves-effect waves-light btn">сохранить записку</a>
-          </div>
-        </div>
+        {
+          renderFooter()
+        }
       </div>
     </div>
   );
