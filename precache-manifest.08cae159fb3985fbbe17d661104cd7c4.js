43fa276699fdede283303adfb487f0316738352b
aleksej <al1111997@yandex.ru>
2020-05-28 20:14:59 +0500

    Updates

---
+++ precache-manifest.08cae159fb3985fbbe17d661104cd7c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5352a100cdeb2333288a63e496385a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8adec63cc0ce49a30500",
+    "url": "./static/css/2.29105dbd.chunk.css"
+  },
+  {
+    "revision": "481110736c8e5d57119e",
+    "url": "./static/css/main.95ad424b.chunk.css"
+  },
+  {
+    "revision": "8adec63cc0ce49a30500",
+    "url": "./static/js/2.4c932fda.chunk.js"
+  },
+  {
+    "revision": "3724a74c11140e34a38d1c473b353315",
+    "url": "./static/js/2.4c932fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481110736c8e5d57119e",
+    "url": "./static/js/main.5b54e0c8.chunk.js"
+  },
+  {
+    "revision": "7e2c39ca3e9fa27c16d7",
+    "url": "./static/js/runtime-main.31f5aea7.js"
+  }
+]);
